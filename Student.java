cf56e95db4f54cbb2a45a66fd73028baf24c0bbc
Nurjamal Osmonalieva <nurjamal7098@gmail.com>
2024-09-19 00:44:50 +0600

    First commit

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    public String name;
+    public String studentID;
+    public String major;
+    public String GPA;
+
+    public void displayInfo () {
+        System.out.println ("Student Name : " + name);
+        System.out.println ("Student ID : " + studentID);
+        System.out.println ("Student Major : " + major);
+        System.out.println ("Student GPA : " + GPA);
+    }
+
+
+}
