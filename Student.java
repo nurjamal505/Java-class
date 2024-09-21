ab733f8dcd45e8f8c1154814d31147e1f9da8450
Nurjamal Osmonalieva <nurjamal7098@gmail.com>
2024-09-22 01:09:43 +0600

    'Constructor'

--- Student.java
+++ Student.java
@@ -1,15 +1,19 @@
 public class Student {
-    public String name;
-    public String studentID;
-    public String major;
-    public String GPA;
+    private String name;
+    private String studentID;
+    private String major;
+    private double GPA;
 
-    public void displayInfo () {
-        System.out.println ("Student Name : " + name);
-        System.out.println ("Student ID : " + studentID);
-        System.out.println ("Student Major : " + major);
-        System.out.println ("Student GPA : " + GPA);
-    }
+public Student(String name, string studentID, string major, double GPA) {
+    this.name = name;
+    this.studentID = studentID;
+    this.major = major;
+    this.GPA = GPA;
+    
 
-
+public Student() {
+    this.name = "";
+    this.studentID = "";
+    this.major = "";
+    this.GPA = 0.0;
 }
